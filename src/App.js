ea157994a38df1832c65f4a4f8a8a8d909356d16
holabisii <ajoseholabisi@gmail.com>
2022-10-28 13:49:14 -0400

    all links configured

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-// - An Anchor tag  styled as a Button with an id **‘btn__zuri’** that redirects to “[https://training.zuri.team/](https://training.zuri.team/)”
-// - An Anchor tag styled as a Button with an id **‘books’** that redirects to [http://books.zuri.team](http://books.zuri.team) with the title and a subtext describing that this is where you find books about design and coding
-// - An Anchor tag styled as a Button with an id **‘book__python’** that redirects to to [https://books.zuri.team](https://books.zuri.team)/python-for-beginners?ref_id=<yourslackname>, with a subtext where you feature the book as if you were selling it.The reference id must be unique and will give you royalties if any sales of the book come
-// - An Anchor tag styled as a Button with an id **‘pitch’** that redirects to to [https://background.zuri.team](https://background.zuri.team), with a subtext where you pitch a service for doing background checks on coders. Use a good, selling sentence.
-// - An Anchor tag styled as a Button with an id **‘book__design’** that redirects to to [https://books.zuri.team/design-rules](https://books.zuri.team/design-rules) with a subtext where you pitch the free design book offered by Zuri.
-
+import { Link } from './components/Link';
 import './index.css'
 
 function App() {
+
+        let linksApi = [
+                {title: 'Twitter Link', link: "https://training.zuri.team/", id : "btn__zuri"},
+                {title: 'Zuri Team', link: "http://books.zuri.team", id : "books"},
+                {title: 'Zuri Books', link: "https://books.zuri.team", id : "book_python"},
+                {title: 'Python Books', link: "https://background.zuri.team", id : "pitch"},
+                {title: 'Background Check for Coders', link: "https://books.zuri.team/design-rules", id : "Background"},
+                {title: 'Design Books', link: "https://books.zuri.team/design-rules", id : "book__design"}
+        ]
+
+        const allLinks = linksApi.map(({title, link, id}) => (
+                <Link title={title} link={link} id={id} key={id} />
+        ))
+
         return (
                 <div className="container w-full">
                         <header className='text-center'>
@@ -18,8 +27,10 @@ function App() {
                                 <h2 className='font-bold text-[20px] text-[#101828] pt-4'>Annette Black</h2>
 
                         </header>
-                        <main>
-                        
+                        <br />
+
+                        <main className='mt-5'>
+                                {allLinks}
                         </main>
                         
                         <footer>
