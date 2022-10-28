5697de8824f937b815cedfa05e2f774472a30ea4
holabisii <ajoseholabisi@gmail.com>
2022-10-28 15:18:50 -0400

    responsiveness done

--- src/App.js
+++ src/App.js
@@ -21,7 +21,8 @@ function App() {
                         <header className='text-center'>
                         
                                 <div className='relative'>
-                                        <img className='border-2 p-3 rounded-full border-dashed absolute -bottom-24 right-80' src="../desktop_Icon.png" alt="navigate_back"  />
+                                        <img className='border-2 p-3 rounded-full border-dashed absolute md:-bottom-24 md:right-40 lg:right-80 hidden md:block' src="../desktop_Icon.png" alt="navigate_back"  />
+                                        <img className='border-2 p-3 rounded-full border-dashed absolute -bottom-24 left-80 pt-4 pb-4 block md:hidden' src="../Icon.png" alt="mobile share button"  />
                                 </div>
                                 <img className='m-auto pt-20' id="profile__img" src="../profile__img.png" alt="profile__img" width={88} />
                                 <h2 className='font-bold text-[20px] text-[#101828] pt-4'>Annette Black</h2>
@@ -32,7 +33,7 @@ function App() {
                         <main className='mt-5'>
 
                                 {allLinks}
-
+                                <br />
                                 <div className='pb-5 pt-0'>
                                         <span className='flex justify-center'>
                                                 <img src="../slack.png" alt="ad" className='p-2' />
@@ -43,10 +44,10 @@ function App() {
                         <br />
                         <br />
                         <br />
-                        <footer className='block md:flex justify-between border-t-2 mx-24 pt-7 pb-10'>
+                        <footer className='block md:flex justify-between border-t-2 mx-5 md:mx-24 pt-7 pb-10'>
                                 
-                                <img src="../Zuri.Internship_Logo.png" alt="zuri intenship logo" width="185px" />
-                                <p className='text-[#667085] text-[16px]'>HNG Internship 9 Frontend Task</p>
+                                <img src="../Zuri.Internship_Logo.png" alt="zuri intenship logo" width="185px" className='pb-4 md:pb-0' />
+                                <p className='text-[#667085] text-[16px] pb-4 md:pb-0'>HNG Internship 9 Frontend Task</p>
                                 <img src="../I4G.png" alt="I4G logo" />
                         
                         </footer>
--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -4,7 +4,7 @@ export const Link = ({title, link, id}) => {
         return (
 
                 <div>
-                        <a href={link} target="_blank" rel="noreferrer" id={id} className="block mx-32 my-6 bg-[#EAECF0] pt-[24px] pr-[32px] pb-[24px] pl-[32px] text-center rounded-md font-medium">
+                        <a href={link} target="_blank" rel="noreferrer" id={id} className="block mx-5 md:mx-32 my-6 bg-[#EAECF0] pt-[24px] pr-[32px] pb-[24px] pl-[32px] text-center rounded-md font-medium">
                                 {title} 
                         </a>
                 </div>
